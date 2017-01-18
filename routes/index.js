5122a8579acbceb1b01bb7c795f5040daa7dd8e8
Malinna Leach <malinnab@yahoo.com>
2017-01-18 12:04:18 +0000

    first test case passing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/data', function(req, res, next) {
+  url = req.query.candidate_url;
+    request(url, function(error, response, html) {
+      if(!error) {
+        var $ = cheerio.load(html);
+        var twitter, github
+        var links = $('body').find('a');
+        for (var i = 1; i < links.length; i++){
+          link = links[i]["attribs"]["href"]
+          if (/^http:\/\/github.com\//.test(link)) {
+            github = link
+          } else if (/^https:\/\/twitter.com\//.test(link)) {
+            twitter = link
+          }
+        }
+        res.render('../views/data.ejs', {
+          github: github,
+          twitter: twitter
+        });
+      };
+    });
 });
 
 module.exports = router;
--- test/feature/data.js
+++ test/feature/data.js
@@ -3,6 +3,7 @@ process.env.NODE_ENV = 'test';
 var app = require('../../app');
 var Browser = require('zombie');
 var http = require("http");
+var assert = require('assert');
 
 describe('data route', function() {
 
@@ -12,11 +13,19 @@ describe('data route', function() {
   });
 
   before(function(done) {
-    this.browser.visit('/data', done);
+    this.browser.visit('/data?candidate_url=http://aakashjapi.com', done);
   });
 
   it('should not throw an error', function() {
      assert.ok(this.browser.success);
+  });
+
+  it('should return Github address', function() {
+    assert.equal(this.browser.text('.git'), 'http://github.com/logicx24');
+  });
+
+  it('should return Twitter address', function() {
+    assert.equal(this.browser.text('.twit'), 'https://twitter.com/logicx24');
   });
 
   after(function(done) {
