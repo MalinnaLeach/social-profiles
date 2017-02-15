eac85c50aed1a670eb7af96c1629ee7a279835a2
Malinna Leach <malinnab@yahoo.com>
2017-02-15 11:30:42 +0000

    draft of recursive check

--- SocialProfiles.js
+++ SocialProfiles.js
@@ -6,19 +6,36 @@ exports.listLinks =  function(html) {
   return $('body').find('a');
 }
 
-exports.checkLinks = function(links) {
-  var twitter, github
+exports.checkLinks = function(links, json={}) {
   for (var i = 0; i < links.length; i++){
     link = links[i]["attribs"]["href"]
     if (/^http(s|):\/\/(github.com|git.io)\//.test(link)) {
-      github = link
+      json.github = link
     } else if (/^http(s|):\/\/twitter.com\//.test(link)) {
-      twitter = link
+      json.twitter = link
     }
   }
-  return {github: github, twitter: twitter};
+  return json;
 }
 
 exports.findLinks =  function(html) {
   return exports.checkLinks(exports.listLinks(html));
+}
+
+exports.findAllLinks = function(url, callback, results={}, visited=[], depth=0) {
+  request(url, function(error, response, html) {
+    if(!error) {
+      if (!visited.includes(url)) {
+        visited.push(url)
+        links = exports.listLinks(html);
+        results = checkLinks(links, results);
+        for (var i = 0; i < links.length; i++){
+          if (depth<3 && links[i].host===window.location.host) {
+            depth += 1;
+            return exports.findAllLinks(links[i], callback, results, visited, depth);
+          }
+        }
+      } else {return results}
+    } else {return results}
+  })
 }
